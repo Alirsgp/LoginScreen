751819fb2f65c9978fa4ab3d3bdd8b48cec8a221
Ali Mohammadian <ali.mohammadian@simoncomputing.com>
2018-07-17 11:03:27 -0400

    Initial Upload

--- LoginScreen/ViewController.swift
+++ LoginScreen/ViewController.swift
@@ -14,7 +14,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    @IBOutlet weak var userText: UITextField!
+    
+    @IBOutlet weak var forgotUserButton: UIButton!
+    
+    @IBOutlet weak var forgotPassButton: UIButton!
+    
+    @IBAction func forgotUserAction(_ sender: Any) {
+        performSegue(withIdentifier: "ForgotUserOrPassword", sender: forgotUserButton)
+    }
+    
+    @IBAction func forgotPassAction(_ sender: Any) {
+        performSegue(withIdentifier: "ForgotUserOrPassword", sender: forgotPassButton)
+    }
+    
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        performSegue(withIdentifier: "RedView", sender: LoginButton)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+        
+        guard let localSender = sender as? UIButton else {return}
+        
+        if (localSender == forgotUserButton) {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else if (localSender == forgotPassButton) {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if (localSender == LoginButton) {
+            segue.destination.navigationItem.title = userText.text
+        }
+    
+    }
 }
 
