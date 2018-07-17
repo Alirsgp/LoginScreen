0cc9dbacef057e2a02ccaa5ddec5dbaf0af9b066
Ali Mohammadian <ali.mohammadian@simoncomputing.com>
2018-07-17 11:06:20 -0400

    Added Change Made

--- LoginScreen/ViewController.swift
+++ LoginScreen/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //CHANGE MADE
     @IBOutlet weak var LoginButton: UIButton!
     
     @IBOutlet weak var userText: UITextField!
